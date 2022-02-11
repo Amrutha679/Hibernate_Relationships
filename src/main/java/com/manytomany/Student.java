ed41f3abc625b657dbc5ef2c3a3ed8df1d0dcfa8
Amrutha <amruthaedu665@gmail.com>
2022-02-11 22:04:02 +0530

    Relationships mapping in hibernate

---
+++ src/main/java/com/manytomany/Student.java
@@ -0,0 +1,56 @@
+package com.manytomany;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.onetomany.Customers;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@Column(name="sid")
+	private int studentId;
+	
+	@Column(name="sname")
+	private String studentName;
+	
+	@ManyToMany(fetch=FetchType.LAZY,targetEntity = Course.class,cascade = CascadeType.ALL)
+	@JoinColumn(name="courseid")
+	private Set obj;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public Set getObj() {
+		return obj;
+	}
+
+	public void setObj(Set obj) {
+		this.obj = obj;
+	}
+	
+}
